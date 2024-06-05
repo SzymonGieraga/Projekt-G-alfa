5e3b8d6f3b1a26fc8e90400245c4cb86a290d759
Jan Boruń <76433910+Ymir41@users.noreply.github.com>
2024-06-05 11:26:47 +0200

    Added user

---
+++ src/main/java/com/projekt_g_alfa/projekt_g_alfa/user/User.java
@@ -0,0 +1,12 @@
+package com.projekt_g_alfa.projekt_g_alfa.user;
+
+import jakarta.validation.constraints.Positive;
+
+public record User(
+        @Positive
+        Integer id,
+        UserType userType,
+        String firstName,
+        String lastName
+) {
+}
---
+++ src/main/java/com/projekt_g_alfa/projekt_g_alfa/user/UserType.java
@@ -0,0 +1,5 @@
+package com.projekt_g_alfa.projekt_g_alfa.user;
+
+public enum UserType {
+    STUDENT, LECTURER, ADMIN
+}
