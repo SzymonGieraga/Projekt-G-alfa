855b59be9c6fde1be222ff01693bc9d5a8973ca9
Jan Boruń <76433910+Ymir41@users.noreply.github.com>
2024-06-05 11:43:37 +0200

    Added basic temporary (in memory) functionality to EventRepository for testing

--- src/main/java/com/projekt_g_alfa/projekt_g_alfa/event/EventRepository.java
+++ src/main/java/com/projekt_g_alfa/projekt_g_alfa/event/EventRepository.java
@@ -1,8 +1,34 @@
 package com.projekt_g_alfa.projekt_g_alfa.event;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class EventRepository {
+    private final List<Event> events = new ArrayList<>();
 
+    public List<Event> findAll(){
+        return events;
+    }
+
+    @PostConstruct
+    private void init(){
+        this.events.add(new Event(
+                1,
+                "Test1",
+                "Lorem ipsum",
+                null,
+                null
+        ));
+        this.events.add(new Event(
+                2,
+                "Test2",
+                "Lorem ipsum",
+                null,
+                null
+        ));
+    }
 }
