b47106e78800f8ad2b4ec72d5fe7c530c614837d
Jan Boruń <76433910+Ymir41@users.noreply.github.com>
2024-06-05 11:13:33 +0200

    Backend initialization

---
+++ src/main/java/com/projekt_g_alfa/projekt_g_alfa/ProjektGAlfaApplication.java
@@ -0,0 +1,13 @@
+package com.projekt_g_alfa.projekt_g_alfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektGAlfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektGAlfaApplication.class, args);
+	}
+
+}
